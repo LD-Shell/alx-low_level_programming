5dc61a3d91a5668775c021618230ee83b14c3298
LD-Shell <alomaradernal@gmail.com>
2022-07-04 05:27:26 +0100

    using puts() to display text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /*header file standard input-output import*/
-int main()
+int main(void)
 {
-	char str[] = "\"Programming is like building a multilingual puzzle\n";
+	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
+
 	return (0);
 }
