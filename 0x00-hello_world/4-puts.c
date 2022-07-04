2c6e19b7acc816e772118527d206602dba75691e
LD-Shell <alomaradernal@gmail.com>
2022-07-04 05:21:09 +0100

    using puts() to display tect

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/*header file standard input-output import*/
+int main()
+{
+	char str[] = "\"Programming is like building a multilingual puzzle\n";
+	puts(str);
+	return (0);
+}
