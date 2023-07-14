4944c2bac3d064e76072cf8c5fbd2e6ebfed38cd
LD-Shell <lanrelangmuir@gmail.com>
2023-07-14 12:52:42 +0800

    Script to print type sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
   *This prints the size of different data types"
   *Entry: Main
-  *Return: 
+  *Return: 0
   */
 int main(void)
 {
@@ -10,6 +10,6 @@ int main(void)
 	printf("Size of a int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
-	return 0;
+	printf("Size of a float: %zu byte(s)", sizeof(float));
+	return (0);
 }
